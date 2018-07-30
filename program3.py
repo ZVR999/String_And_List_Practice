904021b8a52f5f1fcffeb2b4ca8f3f4cbb52d0bf
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-07-30 15:24:04 -0500

    completed compare lists assignment

---
+++ program3.py
@@ -0,0 +1,8 @@
+# python program3.py
+def program(lst,lst2):
+    if lst == lst2:
+        print "The lists are the same"
+    else:
+        print "The lists are not the same"
+
+program([1,2,3,4,5],[1,2,3,4,"snow"])
